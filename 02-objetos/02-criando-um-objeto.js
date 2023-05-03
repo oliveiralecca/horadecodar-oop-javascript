70ea898b750e8360f44accd3126bf0251dbb6bde
Letícia Oliveira <65191407+oliveiralecca@users.noreply.github.com>
2023-05-03 14:37:01 -0300

    how to create objects

---
+++ 02-objetos/02-criando-um-objeto.js
@@ -0,0 +1,17 @@
+/*
+ * Como criar um objeto
+ *  - no JS podemos criar um objeto abrindo e fechando chaves
+ *  - o objeto parece um array de chave e valor
+ *  - podemos atribuir o objeto a uma variável
+*/
+
+let carro = {
+  portas: 4,
+  cor: "Cinza"
+}
+
+let pessoa = {
+  nome: "Letícia"
+}
+
+console.log(typeof carro) // object
