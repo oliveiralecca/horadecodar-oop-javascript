fd1e73aacc2a3547ad4a174310a0902fdb1d346a
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-05-14 23:16:01 -0300

    object literals part 1

---
+++ 02-objetos/20-object-literals-p1.js
@@ -0,0 +1,19 @@
+/*
+ * Object literals
+ *  - função do ES6 que permite criar objetos mais rapidamente
+ *  - utilizando nomes de variáveis para nomes de propriedades, sem redundância caso o nome da prop seja igual ao da variável (não precisa fazer "rodas: rodas")
+*/
+
+let rodas = 4
+let portas = 4
+let aro = 20
+let tetoSolar = true
+
+let carro = {
+  rodas,
+  portas,
+  aro,
+  tetoSolar
+}
+
+console.log(carro) // { rodas: 4, portas: 4, aro: 20, tetoSolar: true }
