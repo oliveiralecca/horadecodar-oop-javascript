dd08653a85de12cf84a9d9a49ccf3345a1651f2e
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-05-17 23:49:07 -0300

    exercise 01

---
+++ 03-exercicios/01-exercicio.js
@@ -0,0 +1,15 @@
+/*
+ * Exercício 1
+ *  - criar um objeto com 3 propriedades
+ *  - a primeira deve ser uma string, a segunda um number e a terceira um boolean
+*/
+
+const garrafa = {
+  material: 'alumínio',
+  capacidade: 500,
+  termica: true
+}
+
+console.log(garrafa.material) // alumínio
+console.log(garrafa.capacidade) // 500
+console.log(garrafa.termica) // true
