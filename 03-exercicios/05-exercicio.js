10e43974d0c3ecba64136793eb9fad5d2904670f
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-05-18 23:38:32 -0300

    exercise 05

---
+++ 03-exercicios/05-exercicio.js
@@ -0,0 +1,23 @@
+/*
+ * Exercício 5
+ *  - criar um objeto Inimigo com as props "nome" e "vivo" (um boolean iniciado como true)
+ *  - o método "atirarshuriken" do exercício passado deve matar o inimigo, setando a prop "vivo" para false
+*/
+
+function Inimigo(nome) {
+  this.nome = nome
+  this.vivo = true
+}
+
+function Ninja(nome) {
+  this.nome = nome
+  this.atirarShuriken = function(inimigo) {
+    inimigo.vivo = false
+  }
+}
+
+const naruto = new Ninja('Naruto', 2)
+const orochimaru = new Inimigo('Orochimaru')
+
+naruto.atirarShuriken(orochimaru)
+console.log(orochimaru.vivo) // false
