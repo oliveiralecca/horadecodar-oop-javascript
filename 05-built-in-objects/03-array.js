cdf1a56246a88d98d1ce78281944026a78dcc01d
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-06 21:26:33 -0300

    object Array

---
+++ 05-built-in-objects/03-array.js
@@ -0,0 +1,22 @@
+/*
+ * Array
+ *  - pai de todos os arrays
+ *  - possui também proprieades e métodos
+ *  - pode instanciar um array com 'new'
+*/
+
+const a = [0, 1, 2]
+const b = new Array(3, 4, 5)
+
+console.log(a) // [ 0, 1, 2 ]
+console.log(b) // [ 3, 4, 5 ]
+
+// Prototype chain: a -> Array -> Object (daqui vem o acesso ao método .toString())
+console.log(a.toString()) // 0,1,2
+console.log(b.toString()) // 3,4,5
+
+console.log(a instanceof Array) // true
+console.log(b instanceof Array) // true
+
+console.log(a instanceof Object) // true
+console.log(b instanceof Object) // true
