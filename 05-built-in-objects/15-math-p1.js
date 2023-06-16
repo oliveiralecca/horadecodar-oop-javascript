1c91eee684845f00edbc0f8135ca7193f251c04a
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-15 23:36:16 -0300

    object math part 1

---
+++ 05-built-in-objects/15-math-p1.js
@@ -0,0 +1,10 @@
+/*
+ * Math — Parte 1
+ *  - um objeto com propriedades e métodos matemáticos
+ *  - podemos saber o valor de PI e até gerar números aleatórios
+*/
+
+console.log(Math.PI) // 3.141592653589793
+console.log(Math.LN2) // 0.6931471805599453 -> logaritmo na base 2
+console.log(Math.random()) // 0.123456789 -> gera um número aleatório entre 0 e 1
+console.log(Math.floor(Math.random() * 101)) // 12 -> gera um número aleatório entre 0 e 100
