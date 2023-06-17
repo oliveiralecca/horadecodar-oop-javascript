907f420f5b0b089cefd682601cc9791cf348e45f
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-17 17:29:19 -0300

    object date part 1

---
+++ 05-built-in-objects/17-date-p1.js
@@ -0,0 +1,9 @@
+/*
+ * Date — Parte 1
+ *  - objeto que lida com datas
+ *  - podemos criar com o 'new' uma data a partir de agora ou a partir de uma data que passarmos
+*/
+
+console.log(new Date()) // 2023-06-17T20:25:04.836Z -> data atual
+console.log(new Date(2024, 10, 31)) // 2024-12-01T03:00:00.000Z
+console.log(new Date(7685768934576)) // 2213-07-21T15:48:54.576Z -> data correspondente ao milissegundo passado
