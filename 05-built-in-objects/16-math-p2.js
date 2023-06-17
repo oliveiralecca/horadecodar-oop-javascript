b816ba2724c4f0da5984f92617174e2dba89ab0f
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-06-17 17:22:10 -0300

    object math part 2

---
+++ 05-built-in-objects/16-math-p2.js
@@ -0,0 +1,13 @@
+/*
+ * Math — Parte 2
+ *  - também tem métodos de arredondamento como: floor(), ceil() e round()
+ *  - métodos para calcular potência e raiz quadrada: pow() e sqrt()
+*/
+
+console.log(Math.floor(12.384)) // 12
+console.log(Math.ceil(12.384)) // 13
+console.log(Math.round(12.384)) // 12
+
+console.log(Math.pow(2, 10)) // 1024
+
+console.log(Math.sqrt(81)) // 9
