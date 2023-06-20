a029d00c11eed5e869d15dcaef0c7f7e8f07efe9
Letícia Oliveira <65191407+oliveiralecca@users.noreply.github.com>
2023-06-20 14:05:41 -0300

    function prop prototype (incomplete)

---
+++ 07-prototype/02-propriedade-prototype.js
@@ -0,0 +1,9 @@
+/*
+ * A propriedade prototype
+ *  - as funções, além de suas propriedades que já vimos, também vêm com a propriedade 'prototype' criada
+ *  - recebemos um objeto vazio, que pode ter propriedades e métodos adicionados
+*/
+
+function abc() {
+    console.log('teste')
+}
