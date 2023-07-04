cfe15fc199cd54aba3ee4f31f4a142dc97f69cfc
Letícia Oliveira <65191407+oliveiralecca@users.noreply.github.com>
2023-07-04 14:49:07 -0300

    check prototypes

---
+++ 07-prototype/11-checando-prototypes.js
@@ -0,0 +1,20 @@
+/*
+ * Checar se é prototype de algum objeto
+ *  - Com o método isPrototypeOf conseguimos checar se um objeto é prototype de outro
+*/
+
+const caracteristicas = {
+    maos: 2,
+    pernas: 2,
+    pes: 2
+}
+
+function Humano(name) {
+    this.name = name
+}
+
+Humano.prototype = caracteristicas
+
+const leticia = new Humano('Letícia')
+console.log(leticia.maos) // 2
+console.log(caracteristicas.isPrototypeOf(leticia)) // true
