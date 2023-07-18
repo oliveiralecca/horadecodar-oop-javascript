855e88730b555b16d990054e3e6c937c87784e97
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-07-18 00:53:28 -0300

    remove tasks method

--- 10-projeto-to-do-list/js/scripts.js
+++ 10-projeto-to-do-list/js/scripts.js
@@ -12,6 +12,33 @@ class Todo {
 
     const list = document.querySelector('#tasks-container');
     list.appendChild(template);
+
+    this.addEvents();
+  }
+
+  removeTask(t) {
+    const taskToRemove = t.parentElement;
+    taskToRemove.remove();
+  }
+
+  completeTask(t) {}
+
+  addEvents() {
+    const allTrashButtons = document.querySelectorAll('.fa-trash');
+    const lastTrashButton = allTrashButtons[allTrashButtons.length - 1];
+
+    // remove task event
+    lastTrashButton.addEventListener('click', function () {
+      todo.removeTask(this);
+    });
+
+    const allCheckButtons = document.querySelectorAll('.fa-check');
+    const lastCheckButton = allCheckButtons[allCheckButtons.length - 1];
+
+    // complete task event
+    lastCheckButton.addEventListener('click', function () {
+      todo.completeTask(this);
+    });
   }
 }
 
