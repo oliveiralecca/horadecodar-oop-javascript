dbc06efe6954dbcc8b94a2d64a9a2626eca70309
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-07-19 00:19:33 -0300

    functions

---
+++ 11-typescript/14-funcoes.ts
@@ -0,0 +1,11 @@
+/*
+ * Funções
+ *  - podemos adicionar os tipos dos parâmetros e do retorno da função
+*/
+
+function soma(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(soma(1, 2)); // 3
+// console.log(soma(1, '2')); // 12 -> Argument of type 'string' is not assignable to parameter of type 'number'
