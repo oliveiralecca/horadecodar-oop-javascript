724fcd36011ac7058245b610eef2076cd74ec5ad
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-07-18 23:21:07 -0300

    tuples

---
+++ 11-typescript/06-tuplas.ts
@@ -0,0 +1,8 @@
+/*
+ * Outros tipos de dados do TS
+ *  - o TS nos provê outros tipos de dados, como: tuplas, Enum, Any, Void, Null e Undefined e Never
+ *  - a tupla serve para determinarmos um conjunto de dados, com tipos específicos e fixos, para uma variável
+*/
+
+let pessoa: [string, number];
+pessoa = ['Luiz', 30];
