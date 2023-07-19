5a87c9570a8809b09da1103e2c5e1e54fcf14908
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-07-18 23:13:21 -0300

    objects and arrays

---
+++ 11-typescript/05-objetos-e-arrays.ts
@@ -0,0 +1,9 @@
+/*
+ * Objetos e Arrays
+ *  - podemos representar o array de duas formas: tipo[] ou Array<tipo>
+*/
+
+const numbers: number[] = [1, 2, 3];
+const words: Array<string> = ['pedra', 'papel', 'tesoura'];
+
+const objeto = { nome: 'Luiz', idade: 30 };
