6bfeaeec258a9c38b08b7e36d930d2b7f41c3d57
oliveiralecca <oliveiraslc@yahoo.com.br>
2023-07-18 23:35:39 -0300

    any

---
+++ 11-typescript/08-any.ts
@@ -0,0 +1,13 @@
+/*
+ * Outros tipos de dados do TS
+ *  - o Enum nos possibilita inserir uma variável com qualquer tipo de dado
+ *  - devemos utilizar em casos extremamente necessários, pois vai contra  a ideia de tipar variáveis
+*/
+
+let qualquerDado: any;
+
+qualquerDado = 'Luiz';
+qualquerDado = 30;
+qualquerDado = true;
+
+const listaIndefinida: any[] = [1, true, 'Luiz'];
